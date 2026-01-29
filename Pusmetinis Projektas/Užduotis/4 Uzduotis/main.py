420bf6e8271e13c71e45ed532fd6f7bb9d14f3be
shizofrenikas <matasxbob@gmail.com>
2026-01-29 17:13:35 +0200

    Update main.py

--- Pusmetinis Projektas/Užduotis/4 Uzduotis/main.py
+++ Pusmetinis Projektas/Užduotis/4 Uzduotis/main.py
@@ -1,12 +1,11 @@
 import sys
 
 
-
 def watchdog():
     return sys.gettrace() is None
 
 def build():
-    data = [102, 108, 97, 103, 123, 114, 97, 109, 117, 107, 97, 105, 110, 111, 115, 125]
+    data = [70, 76, 65, 71, 123, 114, 97, 109, 117, 107, 97, 105, 110, 111, 115, 125]
     return "".join(chr(x) for x in data)
 
 def core():
